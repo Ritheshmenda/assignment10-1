f1b6e5475ded67fb1eb7abdd9536aae15abbbb93
Ritheshmenda <ritheshmenda123@gmail.com>
2021-07-10 14:04:32 +0530

    added: uc6-compute6

--- arthmetic.sh
+++ arthmetic.sh
@@ -1,6 +1,16 @@
+
 #!/bin/bash -x
-read -p "a:" x
-read -p "b:" y
-read -p "c:" z
-compute4=$(($x%$y+$z))
-echo "compute4=$compute4"
+declare -A arthmatic
+read -p "a:" a
+read -p "b:" b
+read -p "c:" c
+compute4=$(($a%$b+$c))
+arthmatic[a]="$a"
+arthmatic[b]="$b"
+arthmatic[c]="$c"
+arthmatic[compute4]="$compute4"
+echo "display a" ${arthmatic[a]}
+echo "display b" ${arthmatic[b]}
+echo "display c" ${arthmatic[c]}
+echo "display compute4" ${arthmatic[compute4]}
+echo "display all" ${!arthmatic[@]}
